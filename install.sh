1abf1078c1f71801e3df0a1ffbf4c4f2b75f22b2
Dylan Pyle <me@dylanpyle.com>
2014-09-25 16:16:48 -0700

    Fix gitconfig for easy checkin

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/rifle.conf ~/.config/ranger/rifle.conf
