d96f3ae2dfe99cd25b853b6e1b34cd0ee1ab4f6b
Dylan Pyle <dylan@ok.local>
2014-09-25 12:04:10 -0700

    Add install script & missing color scheme

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/rifle.conf ~/.config/ranger/rifle.conf
