35368774d686230b50949d8938e8e51040b0a58b
Dylan Pyle <me@dylanpyle.com>
2016-10-25 22:42:23 +0000

    Fix config installation

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ln -s ~/dotfiles/.curlrc ~/.curlrc
 ln -s ~/dotfiles/.psqlrc ~/.psqlrc
 ln -s ~/dotfiles/.vim ~/.config/nvim
 ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+ln -s ~/dotfiles/.vimrc ~/.vim/init.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
