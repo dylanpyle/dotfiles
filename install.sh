ff0455fa7cabb333482911513e8a0bf1a58f21c3
Dylan Pyle <me@dylanpyle.com>
2021-11-30 17:01:28 -0500

    fix install

--- install.sh
+++ install.sh
@@ -7,11 +7,9 @@ ln -s ~/dotfiles/.curlrc ~/.curlrc
 ln -s ~/dotfiles/.psqlrc ~/.psqlrc
 ln -s ~/dotfiles/.vim ~/.config/nvim
 ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vimrc ~/.vim/init.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/gpg-agent.conf ~/.gnupg/gpg-agent.conf
